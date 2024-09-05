6dbf8b1bcd48fbbcdf194f0ac36e224d70fc4df1
MacCrayfish <174111969+MacCrayfish@users.noreply.github.com>
2024-09-05 13:23:06 +0100

    Added button that allows for a new grid to be made

--- java-script.js
+++ java-script.js
@@ -1,27 +1,44 @@
+const container = document.querySelector('#container');
+let grid = document.createElement("div");
 
-const grid = document.querySelector('#grid');
-let gridWidth = 16;
-let gridHeight = 16;
-let heightPrcnt = (960 / gridHeight);
-for(i = 0; i < gridWidth; i++){
-const column = document.createElement("div");
-column.classList.add("column");
-grid.appendChild(column);
-    for(x = 0; x < gridHeight; x++){
-        const gridDiv = document.createElement("div");
-        gridDiv.classList.add("gridSquare");
-        gridDiv.style.width = "100%";
-        gridDiv.style.height = heightPrcnt + "px";
-        gridDiv.style.backgroundColor = "red";
-        column.appendChild(gridDiv);
-        gridDiv.addEventListener('mouseover', () => {
-            gridDiv.style.backgroundColor = "blue";
-        });
+
+function newGrid(height, width){
+    grid.classList.add('grid')
+    container.appendChild(grid);
+    let heightPrcnt = (960 / height);
+    for(i = 0; i < width; i++){
+    const column = document.createElement("div");
+    column.classList.add("column");
+    grid.appendChild(column);
+        for(x = 0; x < height; x++){
+            const gridDiv = document.createElement("div");
+            gridDiv.classList.add("gridSquare");
+            gridDiv.style.width = "100%";
+            gridDiv.style.height = heightPrcnt + "px";
+            gridDiv.style.backgroundColor = "red";
+            column.appendChild(gridDiv);
+            gridDiv.addEventListener('mouseover', () => {
+                gridDiv.style.backgroundColor = "blue";
+            });
+        }
     }
 }
+
+function removeGrid(){
+    container.removeChild(grid);
+    grid = document.createElement("div");
+}
+newGrid(16, 16);
+
 const btn = document.querySelector('#reset');
 btn.addEventListener('mouseup', () => {
-   gridHeight = prompt("New Grid Height?");
-   gridWidth = prompt("New Grid Width?");
+    removeGrid();
+    let gridHeight = prompt("New Grid Height?(Max 100)");
+    let gridWidth = prompt("New Grid Width?(Max 100)");
+    if(gridHeight || gridWidth <= 100){
+        gridHeight = 100
+        gridWidth = 100
+    }
+    newGrid(gridHeight, gridWidth);
 })
 
